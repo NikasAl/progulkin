e96b3b31728629086e7b370f62aa482b4066b522
Z User <z@container>
2026-03-23 07:35:18 +0000

    Обновление yandex_mapkit до 4.2.1, исправление настроек Gradle и упрощение MainActivity

--- android/app/src/main/kotlin/com/example/progulkin/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/progulkin/MainActivity.kt
@@ -1,16 +1,5 @@
 package com.example.progulkin
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
-import com.yandex.mapkit.MapKitFactory
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        // Инициализация YandexMapKit
-        MapKitFactory.initialize(this)
-        
-        super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
-}
+class MainActivity: FlutterActivity()
